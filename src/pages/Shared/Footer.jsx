cc395ef2a813066e64a7625440c5b01843e8e86b
rafi0012 <rafiultalukdar007@gmail.com>
2025-12-06 11:55:01 +0600

    File setup, Added Login page, Register page, NavBar & Footer

---
+++ src/pages/Shared/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaMountainCity } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { FaFacebookSquare, FaInstagram, FaPhoneVolume } from 'react-icons/fa';
+import { IoLocation } from 'react-icons/io5';
+import { BsTwitterX } from 'react-icons/bs';
+
+const Footer = () => {
+    return (
+        <>
+            <div className='bg-[#FBFCFB] dark:bg-gray-900 shadow-[0_-3px_6px_rgba(0,0,0,0.1)]'>
+                <div className='container'>
+                    <div className='py-[35px] grid grid-cols-12 gap-5 gap-y-[30px] border-b border-[#219E64]'>
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-4 max-w-[320px]'>
+                            <button className='flex items-center gap-1.5 text-[20px] sm:text-[22px] font-bold text-[#219E64]'><FaMountainCity /><span>CityFix</span></button>
+                            <p className='text-[18px] text-black pt-5 dark:text-[#E9F7EF]'>CityFix brings energy, elegance, and unforgettable moments to every celebration, making each event truly special and memorable.</p>
+                        </div>
+
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
+                            <h4 className='text-[21px] text-black dark:text-white font-semibold pb-[15px]'>Quick Links</h4>
+                            <ul className='flex flex-col gap-3'>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium'>Services</li>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium'>Contact Us</li>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium'>Privacy Policy</li>
+                            </ul>
+                        </div>
+
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-3'>
+                            <h4 className='text-[21px] text-black dark:text-white font-semibold pb-[15px]'>Contact Us</h4>
+                            <ul className='flex flex-col gap-3'>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium flex items-center gap-[7px]'><IoIosMail /><span>info@cityfix.com</span></li>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium flex items-center gap-[7px]'><FaPhoneVolume /><span>+880 1XXX-XXXXXX</span></li>
+                                <li className='text-[18px] text-[#141414] dark:text-[#E9F7EF] font-medium flex items-center gap-[7px]'><IoLocation /><span>Dhaka, Bangladesh</span></li>
+                            </ul>
+                        </div>
+
+                        <div className='col-span-12 sm:col-span-6 lg:col-span-2'>
+                            <h4 className='text-[21px] text-black dark:text-white font-semibold pb-[15px]'>Follow Us</h4>
+                            <ul className='flex items-center gap-6'>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><FaInstagram size={30} /></li>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><FaFacebookSquare size={30} /></li>
+                                <li className='text-[#219E64] hover:text-[#069251] duration-300 cursor-pointer'><BsTwitterX size={28} /></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='py-7 text-center'>
+                        <p className='text-[#141414] text-[16px] dark:text-[#E9F7EF]'>© 2025 CityFix. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Shared/Nav.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router';
+import { FaMountainCity } from "react-icons/fa6";
+import { AiOutlineLogout } from "react-icons/ai";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { CgProfile } from "react-icons/cg";
+import useAuth from '../../hooks/useAuth';
+
+const Nav = () => {
+
+    const { user, logOut } = useAuth();
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+    const [open, setOpen] = useState(false);
+
+    const links = (
+        <>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/all-issues">All Issues</NavLink>
+            <NavLink to="/how-it-works">How It Works</NavLink>
+            <NavLink to="/about-us">About Us</NavLink>
+        </>
+    );
+
+    // Log Out
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
+
+    // Dark mood
+    useEffect(() => {
+        const html = document.querySelector("html");
+        html.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const handleTheme = (checked) => {
+        setTheme(checked ? "dark" : "light");
+    };
+
+    console.log(user)
+
+
+    return (
+        <>
+            <div className='bg-[#F8F8F8] dark:bg-gray-900 shadow-sm dark:shadow-md py-2.5 sticky top-0 z-99'>
+                <div className=''>
+                    <div className='navbar container'>
+                        <div className='navbar-start'>
+                            <div className='dropdown'>
+                                <div tabIndex={0} role='button' className='lg:hidden cursor-pointer mr-3.5'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-10' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' /> </svg>
+                                </div>
+                                <nav tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-2 mt-3 w-52 p-2 shadow px-5 py-3 space-y-1.5">
+                                    {links}
+                                </nav>
+                            </div>
+                            <NavLink to='/' className='flex items-center gap-1.5 text-[20px] sm:text-[22px] lg:text-[22px] font-bold text-[#219E64]'><FaMountainCity /><span>CityFix</span></NavLink>
+                        </div>
+                        <div className='navbar-center hidden lg:flex'>
+                            <nav className='flex items-center gap-[22px]'>
+                                {links}
+                            </nav>
+                        </div>
+                        <div className='navbar-end gap-3'>
+                            <div>
+                                <input onChange={(e) => handleTheme(e.target.checked)} type="checkbox" defaultChecked={localStorage.getItem('theme') === "dark"} className="toggle"/>
+                            </div>
+                            <div>
+                                {
+                                    user ? 
+                                    <>
+                                        <div className='relative'>
+                                            <img onClick={() => setOpen(!open)} src={user.photoURL} className='w-11 h-11 object-cover rounded-full cursor-pointer p-0.5 border border-[#219e64a8]' alt="" />
+                                            {
+                                                open && (
+                                                    <div className='absolute menu right-0 w-[235px] pt-[18px] pb-2 rounded-lg space-y-3 bg-[#F8F8F8] dark:bg-gray-900 shadow-sm dark:shadow-md'>
+                                                        <div className='px-[15px]'>
+                                                            <h3 className='text-[#141414] dark:text-white text-[20px] font-semibold'>{user.displayName}</h3>
+                                                            <p className='text-sm '>{user.email}</p>
+                                                        </div>
+
+                                                        <div onClick={() => setOpen(false)} className='border-t border-b border-[#DADADA] py-4 dark:border-[#464646] flex flex-col'>
+                                                            <Link to='/' className='flex items-center gap-2 px-[15px] py-2 text-[15px] font-medium text-[#141414] dark:text-white hover:bg-[#219E64] hover:text-white transition rounded w-full'><LuLayoutDashboard /><span>Dashboard</span></Link>
+                                                            <Link to='/' className='flex items-center gap-2 px-[15px] py-2 text-[15px] font-medium text-[#141414] dark:text-white hover:bg-[#219E64] hover:text-white transition rounded w-full'><CgProfile /><span>Profile</span></Link>
+                                                        </div>
+
+                                                        <div onClick={() => setOpen(false)} className='dropdown-link'>
+                                                            <button onClick={() => handleLogOut()} className='flex items-center gap-2 px-[15px] py-1.5 text-[16px] font-medium text-red-500 hover:bg-[#219E64] hover:text-white transition rounded w-full'><AiOutlineLogout /><span>LogOut</span></button>
+                                                        </div>
+                                                    </div>)
+                                            }
+                                        </div>
+                                    </>
+                                    :
+                                    <div className='login-nav flex items-center gap-2'>
+                                        <NavLink to='/login' className='py-1.5 px-5 bg-white rounded-lg text-[#219E64] border border-[#219E64] text-[18px] font-semibold'>Login</NavLink>
+                                        {/* <NavLink to='/register' className='py-1.5 px-5 bg-white rounded-lg text-[#219E64] border border-[#219E64] text-[18px] font-semibold hidden sm:block'>Register</NavLink> */}
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Nav;
