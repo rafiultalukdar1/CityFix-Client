0344713809479c9cadfc1e8eda5c42b8d98e44ed
rafi0012 <rafiultalukdar007@gmail.com>
2025-12-13 12:55:07 +0600

    Added ReportIssue Page

--- src/pages/CitizenDashboard/MyProfile.jsx
+++ src/pages/CitizenDashboard/MyProfile.jsx
@@ -67,61 +67,62 @@ const MyProfile = () => {
     };
 
     return (
-        <div className='px-3 pt-12'>
-            <div className='max-w-[700px] mx-auto flex flex-col gap-8'>
-                <div>
-                    <h2 className='text-[32px] md:text-[38px] lg:text-[48px] text-[#141414] dark:text-white font-bold'>My Profile</h2>
-                    <p className='text-[#6D7873] dark:text-[#E7F8F2] text-[15px]'>Manage your account settings and subscription.</p>
-                </div>
-
-                <div className='bg-[#FBFCFB] dark:bg-gray-900 shadow-sm py-3.5 md:py-8 px-4 md:px-8 rounded-lg lg:rounded-xl flex justify-between items-center'>
-                    <div className='flex items-center gap-3.5'>
-                        <div className='bg-[#E7F8F2] text-[#10B77F] p-2 md:p-4 rounded-md text-[22px] md:text-[28px]'>
-                            <FaRegChessQueen />
-                        </div>
-                        <div>
-                            <h3 className='text-[20px] md:text-[24px] font-bold'>Free Plan</h3>
-                            <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Limited to 3 issue reports</p>
-                        </div>
+        <>
+            <title>CityFix - My Profile</title>
+            <div className='px-3 pt-12'>
+                <div className='max-w-[700px] mx-auto flex flex-col gap-8'>
+                    <div>
+                        <h2 className='text-[32px] md:text-[38px] lg:text-[48px] text-[#141414] dark:text-white font-bold'>My Profile</h2>
+                        <p className='text-[#6D7873] dark:text-[#E7F8F2] text-[15px]'>Manage your account settings and subscription.</p>
                     </div>
-                    <button className='px-3.5 py-1.5 rounded-md font-medium bg-[#219E64] hover:bg-[#0c7e49] transition text-white'>Upgrade</button>
-                </div>
 
-                <div className='bg-[#FBFCFB] dark:bg-gray-900 shadow-sm py-3.5 md:py-8 px-4 md:px-8 rounded-lg lg:rounded-xl flex flex-col gap-2.5'>
-                    <div className='flex items-center gap-5'>
-                        <img src={user?.photoURL || "/default-avatar.png" } alt="Profile" className="w-15 sm:w-18 h-15 sm:h-18 rounded-full object-cover p-0.5 border border-[#219E64]" />
-                        <div>
-                            <h4 className='text-[20px] font-semibold text-[#141414] dark:text-white'>{profile.name}</h4>
-                            <p className='text-[#6D7873] text-[15px]'>{profile.email}</p>
-                        </div>
-                    </div>
-                    <form onSubmit={handleSubmit(handleUpdate)} className='flex flex-col gap-2.5'>
-                        <div>
-                            <label className='form-label'>Name</label>
-                            <input type="text" defaultValue={profile.name} {...register('name', { required: 'Name is required' })} className='form-input' /> {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
-                        </div>
-                        <div>
-                            <label className='form-label'>Profile Photo</label>
-                            <input type="file" {...register('photo')} className='file-input w-full text-[#464545] text-[15px] outline-0 border border-[#219e648f] focus:border-[#219E64] bg-[#F3F3F3] rounded dark:text-[#F3F3F3] dark:bg-gray-900 dark:border-[#219E6480] dark:focus:border-[#219E64]' /> {errors.photo && <p className="text-red-500 text-sm">{errors.photo.message}</p>}
-                        </div>
-
-                        <div className='grid grid-cols-1 sm:grid-cols-2 gap-3 bg-white dark:bg-[#1D232A] shadow-sm rounded-lg py-5 px-4 mt-3'>
-                            <div>
-                                <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Member Since:</p>
-                                <p className='flex items-center gap-1.5 text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'><IoIosCalendar /><span>{new Date(profile.createdAt).toLocaleDateString('en-GB', {day: '2-digit', month: 'short', year: 'numeric'})}</span></p>
+                    <div className='bg-[#FBFCFB] dark:bg-gray-900 shadow-sm py-3.5 md:py-8 px-4 md:px-8 rounded-lg lg:rounded-xl flex justify-between items-center'>
+                        <div className='flex items-center gap-3.5'>
+                            <div className='bg-[#E7F8F2] text-[#10B77F] p-2 md:p-4 rounded-md text-[22px] md:text-[28px]'>
+                                <FaRegChessQueen />
                             </div>
                             <div>
-                                <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Designation:</p>
-                                <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2] flex items-center gap-1.5'><FaRegUserCircle /><span>{profile.role}</span></p>
+                                <h3 className='text-[20px] md:text-[24px] font-bold'>Free Plan</h3>
+                                <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Limited to 3 issue reports</p>
                             </div>
                         </div>
+                        <button className='px-3.5 py-1.5 rounded-md font-medium bg-[#219E64] hover:bg-[#0c7e49] transition text-white'>Upgrade</button>
+                    </div>
 
+                    <div className='bg-[#FBFCFB] dark:bg-gray-900 shadow-sm py-3.5 md:py-8 px-4 md:px-8 rounded-lg lg:rounded-xl flex flex-col gap-2.5'>
+                        <div className='flex items-center gap-5'>
+                            <img src={user?.photoURL || "/default-avatar.png" } alt="Profile" className="w-15 sm:w-18 h-15 sm:h-18 rounded-full object-cover p-0.5 border border-[#219E64]" />
+                            <div>
+                                <h4 className='text-[20px] font-semibold text-[#141414] dark:text-white'>{profile.name}</h4>
+                                <p className='text-[#6D7873] text-[15px]'>{profile.email}</p>
+                            </div>
+                        </div>
+                        <form onSubmit={handleSubmit(handleUpdate)} className='flex flex-col gap-2.5'>
+                            <div>
+                                <label className='form-label'>Name</label>
+                                <input type="text" defaultValue={profile.name} {...register('name', { required: 'Name is required' })} className='form-input' /> {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+                            </div>
+                            <div>
+                                <label className='form-label'>Profile Photo</label>
+                                <input type="file" {...register('photo')} className='file-input w-full text-[#464545] text-[15px] outline-0 border border-[#219e648f] focus:border-[#219E64] bg-[#F3F3F3] rounded dark:text-[#F3F3F3] dark:bg-gray-900 dark:border-[#219E6480] dark:focus:border-[#219E64]' /> {errors.photo && <p className="text-red-500 text-sm">{errors.photo.message}</p>}
+                            </div>
 
-                        <button type='submit' disabled={loading} className='py-1.5 w-full bg-[#219E64] hover:bg-[#0c7e49] transition rounded mt-3 text-white text-[18px] font-medium disabled:opacity-50'> {loading ? 'Updating...' : 'Update Profile'} </button>
-                    </form>
+                            <div className='grid grid-cols-1 sm:grid-cols-2 gap-3 bg-white dark:bg-[#1D232A] shadow-sm rounded-lg py-5 px-4 mt-3'>
+                                <div>
+                                    <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Member Since:</p>
+                                    <p className='flex items-center gap-1.5 text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'><IoIosCalendar /><span>{new Date(profile.createdAt).toLocaleDateString('en-GB', {day: '2-digit', month: 'short', year: 'numeric'})}</span></p>
+                                </div>
+                                <div>
+                                    <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2]'>Designation:</p>
+                                    <p className='text-[16px] text-[#6D7873] dark:text-[#E7F8F2] flex items-center gap-1.5'><FaRegUserCircle /><span>{profile.role}</span></p>
+                                </div>
+                            </div>
+                            <button type='submit' disabled={loading} className='py-1.5 w-full bg-[#219E64] hover:bg-[#0c7e49] transition rounded mt-3 text-white text-[18px] font-medium disabled:opacity-50'> {loading ? 'Updating...' : 'Update Profile'} </button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/pages/CitizenDashboard/ReportIssue.jsx
+++ src/pages/CitizenDashboard/ReportIssue.jsx
@@ -1,11 +1,116 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router';
 
 const ReportIssue = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = async (data) => {
+        try {
+            let imageURL = null;
+            if (data.photo && data.photo[0]) {
+                const formData = new FormData();
+                formData.append('image', data.photo[0]);
+                const imgRes = await axios.post(
+                    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_img_host_KEY}`,
+                    formData
+                );
+                imageURL = imgRes.data.data.display_url;
+            }
+            const issueData = {
+                title: data.title,
+                description: data.description,
+                category: data.category,
+                location: data.location,
+                images: imageURL,
+                priority: "normal",
+                isBoosted: false,
+                status: "pending",
+                submittedBy: user?.uid || user?.email,
+                assignedStaff: null,
+                upvotes: 0,
+                organizer_name: user?.displayName,
+                organizer_email: user?.email,
+                organizer_photo: user?.photoURL || null,
+                timeline: [
+                    {
+                        status: "pending",
+                        message: "Citizen reported an issue.",
+                        updatedBy: {
+                            name: user?.displayName,
+                            role: "citizen"
+                        },
+                        timestamp: new Date().toISOString()
+                    }
+                ],
+                createdAt: new Date().toISOString()
+            };
+            const res = await axiosSecure.post('/issues', issueData);
+            if (res.data.insertedId) {
+                toast.success('Issue reported successfully!');
+                reset();
+                navigate('/dashboard/my-issues');
+            }
+        } catch (error) {
+            toast.error('Failed to submit report. Please try again.', error);
+        }
+    };
+
     return (
         <>
+            <title>CityFix - Report Issue</title>
             <div className='px-3 pt-12'>
-                <div className='max-w-[1500px] mx-auto bg-[#FBFCFB] dark:bg-gray-900 shadow-sm py-10 px-8 rounded-lg lg:rounded-xl'>
-                    Report Issue
+                <div className='max-w-[820px] mx-auto'>
+                    <h2 className='text-center text-[32px] sm:text-[40px] lg:text-[50px] font-bold'> Create a new report </h2>
+                    <p className='text-[#6D7873] dark:text-[#E7F8F2] text-[15px] text-center'> Report a public issue with details so it can be reviewed and resolved promptly. </p>
+
+                    <div className='py-7 px-5 border border-[#CCCCCC] dark:bg-gray-900 rounded-2xl mt-5 lg:mt-8'>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                            <div>
+                                <input {...register('title', { required: 'Title is required' })} placeholder="Title" className="form-input w-full" /> {errors.title && <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>}
+                            </div>
+                            <div>
+                                <textarea {...register('description', { required: 'Description is required' })} placeholder="Description" rows="5" className="form-input w-full" /> {errors.description && <p className="text-red-500 text-sm mt-1">{errors.description.message}</p>}
+                            </div>
+                            <div>
+                                <select {...register('category', { required: 'Please select a category' })} className="form-input w-full">
+                                    <option value="">Select Category</option>
+                                    <option value="electricity">Electricity</option>
+                                    <option value="bridge">Bridge</option>
+                                    <option value="waste">Waste</option>
+                                    <option value="footpath">Footpath</option>
+                                    <option value="public park">Public Park</option>
+                                    <option value="natural">Natural</option>
+                                    <option value="obstruction">Obstruction</option>
+                                    <option value="drainage">Drainage</option>
+                                    <option value="water leakage">Water Leakage</option>
+                                    <option value="pothole">Pothole</option>
+                                    <option value="streetlight">Streetlight</option>
+                                    <option value="road">Road</option>
+                                    <option value="traffic sign">Traffic Sign</option>
+                                    <option value="garbage">Garbage</option>
+                                </select> {errors.category && <p className="text-red-500 text-sm mt-1">{errors.category.message}</p>}
+                            </div>
+                            <div>
+                                <input {...register('location', { required: 'Location is required' })} placeholder="Location" className="form-input w-full" /> {errors.location && <p className="text-red-500 text-sm mt-1">{errors.location.message}</p>}
+                            </div>
+                            <div>
+                                <input type="file" accept="image/*" {...register('photo', { required: 'Image is required' })} className="form-input w-full" /> {errors.photo && <p className="text-red-500 text-sm mt-1">{errors.photo.message}</p>}
+                            </div>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                                <input value={user?.displayName || '' } disabled className="form-input w-full bg-gray-100 dark:bg-gray-800" />
+                                <input value={user?.email || '' } disabled className="form-input w-full bg-gray-100 dark:bg-gray-800" />
+                            </div>
+                            <button type="submit" className="w-full bg-[#219E64] hover:bg-[#0c7e49] text-white py-3 rounded font-semibold"> Submit Report </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
